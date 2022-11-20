afc66a254270cc83ca579ac725c02a4b74aa29a7
codinggithub111 <118640522+codinggithub111@users.noreply.github.com>
2022-11-20 00:21:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var name = window.prompt("enter your slang word, all lowercase please, and reload site to learn new definition");
+let ok = prompt("enter your slang word");
+let name = ok.toLowerCase();
 
 if (name == "bussin")
     document.write("to mean that something is delicious");
@@ -6,11 +7,11 @@ else if (name == "thingamabob")
     document.write("a place word for thing or something that you do not know");
 else if (name == "slay")
     document.write("to mean that you did something great");
-else if (name == "FR")
+else if (name == "fr")
     document.write("stands for, 'for real'");
 else if (name == "cap")
     document.write("to mean someone is lying");
-else if (name == "cap")
+else if (name == "wsg")
     document.write("to mean someone is lying");
 else if (name == "ong")
     document.write("stands for on god");
@@ -25,26 +26,26 @@ else if (name == "mood")
 else if (name == "lit")
     document.write("when something is great or awesome");
 else if (name == "hits different")
-    document.write("when something inspire you");
+    document.write("when something inspires you");
 else if (name == "main character")
     document.write("when someone is so great or egotistical they seem like they are the main character from a book or story");
 else if (name == "slaps")
     document.write("to mean something is really good");
 else if (name == "simp")
-    document.write("to mean someone does anything for someone they like");
+    document.write("when someone does anything for someone they like");
 else if (name == "vibe")
     document.write("when someone has a feeling about something");
 else if (name == "canceled")
     document.write("when someone does something wrong (normally morally wrong) they become canceled");
-else if (name == "L")
+else if (name == "l")
     document.write("it means loss or you tooks a loss");
-else if (name == "W")
+else if (name == "w")
     document.write("it means win or you won");
-else if(name == "TFW")
+else if(name == "tfw")
     document.write("means that feeling when");
 else if (name == "yessir")
     document.write("to express agreement");
 else if (name == "ok boomer")
-    document.write("to say rudely say yes to an old person (its not that rude just easiest way to explain");
+    document.write("to say rudely say yes to an old person (its not that rude just easiest way to explain)");
 else
     document.write("slang definition not available please contact owner");
