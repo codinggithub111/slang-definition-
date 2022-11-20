a856be3a978fbe34ce605b8ec45b9fbfc6a2c1e8
codinggithub111 <118640522+codinggithub111@users.noreply.github.com>
2022-11-19 18:04:16 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+var name = window.prompt("enter your slang word, all lowercase please, and reload site to learn new definition");
+
+if (name == "bussin")
+    document.write("to mean that something is delicious");
+else if (name == "thingamabob")
+    document.write("a place word for thing or something that you do not know");
+else if (name == "slay")
+    document.write("to mean that you did something great");
+else if (name == "FR")
+    document.write("stands for, 'for real'");
+else if (name == "cap")
+    document.write("to mean someone is lying");
+else if (name == "cap")
+    document.write("to mean someone is lying");
+else if (name == "ong")
+    document.write("stands for on god");
+else if (name == "bet")
+    document.write("to mean sure or to understand");
+else if (name == "lowkey")
+    document.write("to do something quietly or modestly");
+else if (name == "tea")
+    document.write("means gossip");
+else if (name == "mood")
+    document.write("when something is relatable");
+else if (name == "lit")
+    document.write("when something is great or awesome");
+else if (name == "hits different")
+    document.write("when something inspire you");
+else if (name == "main character")
+    document.write("when someone is so great or egotistical they seem like they are the main character from a book or story");
+else if (name == "slaps")
+    document.write("to mean something is really good");
+else if (name == "simp")
+    document.write("to mean someone does anything for someone they like");
+else if (name == "vibe")
+    document.write("when someone has a feeling about something");
+else if (name == "canceled")
+    document.write("when someone does something wrong (normally morally wrong) they become canceled");
+else if (name == "L")
+    document.write("it means loss or you tooks a loss");
+else if (name == "W")
+    document.write("it means win or you won");
+else if(name == "TFW")
+    document.write("means that feeling when");
+else if (name == "yessir")
+    document.write("to express agreement");
+else if (name == "ok boomer")
+    document.write("to say rudely say yes to an old person (its not that rude just easiest way to explain");
+else
+    document.write("slang definition not available please contact owner");
